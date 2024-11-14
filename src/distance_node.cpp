34e128982fc04acfc616ae8a7f343dbf77191f86
ChiaraBuono <7687956@studenti.unige.it>
2024-11-14 09:44:04 +0000

    creation distance_node

---
+++ src/distance_node.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Spawn.h"
+#include "turtlesim/Kill.h"
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+    //checks the relative distance between turtle1 and turtle2
+
+
+    /* publish on a topic the distance 
+    (you can use a std_msgs/Float32 for that)*/
+
+
+    /*stops the moving turtle if the two turtles are “too close” 
+    (you may set a threshold to monitor that)*/
+
+
+    /*stops the moving turtle if the position 
+    is too close to the boundaries 
+    (.e.g, x or y > 10.0, x or y < 1.0) */
+
+
+    return 0;
+}
